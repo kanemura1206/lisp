13d9c207f560accd0e1968ede9e0c2626c0747e4
kanemura1206 <kanemura1206@gmail.com>
2012-04-06 14:15:15 +0900

    second commit

---
+++ sample.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main()
+{
+  char *str = "abc";
+
+  //  int i;
+  //  for (i = 0; str[i] != '\0'; i++) {
+  //    printf("%c\n", str[i]);
+  //  }
+
+  return 0;
+}
+
+
+//strcmp
+//strlen
+//strtok
