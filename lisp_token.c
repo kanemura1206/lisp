5c6101b42add1581694775c3e4a0f52313f48768
kanemura1206 <kanemura1206@gmail.com>
2012-05-24 17:47:43 +0900

    update token

--- lisp_token.c
+++ lisp_token.c
@@ -15,38 +15,31 @@ char** split(char* formula)
 	char **token = (char **)calloc(len,sizeof(char*));
 	int i,j,k;
 	j = 0; k = 0;
-	int askii = formula[j];
 	do {
-		if (askii > 32 && askii != 40 && askii != 41) {
+		if ((int)formula[j] > 32) {
 			char *str = (char *)calloc(len,sizeof(char));
 			i = 0;
-			do {
-				str[i] = formula[j];
-				i++; j++;
-				askii = formula[j];
-			} while (askii > 32 && askii != 40 && askii != 41);
+			if (formula[j] == '(' || formula[j] == ')') {
+				str[0] = formula[j];
+				j++;
+			}
+			else {
+				do {
+					str[i] = formula[j];
+					i++; j++;
+				} while ((int)formula[j] > 32 && formula[j] != '(' && formula[j] != ')');
+			}
 			int slen = strlen(str);
 			token[k]  = malloc(sizeof(char)*(slen+1));
 			strcpy(token[k],str);
 			free(str);
 			k++;
-		}
-		else if (askii == 40 || askii == 41) {
-			char *str = (char *)calloc(len,sizeof(char));
-			str[0] = formula[j];
-			int slen = strlen(str);
-			token[k] = malloc(sizeof(char)*(slen + 1));
-			strcpy(token[k],str);
-			free(str);
-			j++; k++;
-			askii = formula[j];
 			token[k] = NULL;
 		}
-		else if (askii < 33) {
+		else if ((int)formula[j] < 33 && formula[j] != '\0') {
 			j++;
-			askii = formula[j];
 		}
-	} while (askii != 0);
+	} while (formula[j] != '\0');
 	return token;
 }
 
